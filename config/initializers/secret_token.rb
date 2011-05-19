99e0e139e1e3116135ffe69348433f0f278c35dc
Maharba <maharba@MZ.(none)>
2011-05-19 02:28:21 -0500

    Primer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniNet::Application.config.secret_token = 'b4d5d0d7d0a15e11d9e82d6c2117203e97db6d619ca907ff2f0a58916a01efa94fb8976aa79f08a28f5ec96ec4f8fd818cd7329a60371c862a2cf6ccab759f83'
